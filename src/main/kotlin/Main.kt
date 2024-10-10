e39e5a4759b87ca5f78657a1ae26e1d841d26317
Zaldi Irvana <akurizaldirv@gmail.com>
2024-10-10 15:20:44 +0700

    fundamental

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package xyz.zaldev
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Zaldi"
+    var birthYear = 2002
+    var age = 2024 - birthYear
+
+    println("Hello, my name is $name and I am $age years old")
+}
