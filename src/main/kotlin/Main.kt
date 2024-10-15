89c288399548b40e56613d4893a3caae50f2ed26
Zaldi Irvana <akurizaldirv@gmail.com>
2024-10-15 11:08:32 +0700

    string

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,35 @@ package xyz.zaldev
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Zaldi"
-    var birthYear = 2002
-    var age = 2024 - birthYear
+    var character: Char = 'A'
+    println(character)
+    println(++character) // incrementing character unicode and print
 
-    println("Hello, my name is $name and I am $age years old")
+    println("\n")
+
+    // string indexing
+    val i = 2
+    val text = "Hello World!"
+    val third = text[3]
+
+    println("The third letter of $text is $third")
+    for (i in text) {
+        println(i)
+    }
+
+    // escaped string
+    println("This is how to write \"Hello World!\" with string")
+
+    println()
+    println("""
+        this is raw string
+        the indentation
+        is still here
+    """)
+
+    println("""
+        this is raw string
+        without
+        the indentation
+    """.trimIndent())
 }
