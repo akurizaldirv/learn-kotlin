c8c67644c5b453ddb93ca95a324907edf9ddd6eb
Zaldi Irvana <akurizaldirv@gmail.com>
2024-10-24 15:20:13 +0700

    add secondary constructor

---
+++ src/main/kotlin/Plant.kt
@@ -0,0 +1,24 @@
+package xyz.zaldev
+
+class Plant (private var name: String, private var family: String, private var description: String?, private var height: Int?) {
+    private var isExtinct: Boolean
+
+    init {
+        this.name = name
+        this.family = family
+        this.description = description
+        this.height = if (height!! > 0) height else 0 // height!! = is not null
+        this.isExtinct = false
+    }
+
+    // secondary constructor
+    // if the property already inside the class assignment, it should be written in return block to call the this()
+    constructor(name: String, family: String, description: String, height: Int, isExtinct: Boolean) : this(name, family, description, height) {
+        this.isExtinct = isExtinct
+    }
+
+    // class can have more than one secondary constructor
+    constructor(name: String, family: String, isExtinct: Boolean) : this(name, family, description = null, height = null) {
+        this.isExtinct = false
+    }
+}
