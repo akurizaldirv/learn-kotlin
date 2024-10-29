6fad5cda3bae1b30e9b8058705102aa9611bd0fe
Zaldi Irvana <akurizaldirv@gmail.com>
2024-10-29 09:46:46 +0700

    add map

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -74,54 +74,95 @@ fun main() {
 //    gradeList[0] = 99
 //    println(gradeList)
 
+//
+//    // set
+//    // Set<*> is Immutable
+//    // MutableSet<*> is Mutable
+//    // same as list, but every content in it must be unique
+//    val car1 = Car(
+//        name = "Hyundai",
+//        fuel = "Gasoline"
+//    )
+//    val car2 = car1
+//
+//    // immutable
+//    val mixedSet1 = setOf(1, "True", 2.3, true, car1, car2) // cause car1 and car2 have the same reference, only one will included
+//    val intSet1: Set<Int> = setOf(1, 2, 3, 4, 5, 6, 1, 2, 3, 4) // 1, 2, 3, 4, 5, 6
+//    val intSet2: Set<Int> = setOf(5, 6, 1, 2, 3, 4) // 1, 2, 3, 4, 5, 6
+//    val intSet3: Set<Int> = setOf(5, 10, 20, 30, 40, 50)
+//
+//    println(mixedSet1)
+//    println(intSet1)
+//    println(intSet2)
+//    println(intSet3)
+//    println(intSet2 == intSet1) // true
+//    println(intSet2 == intSet3) // true
+//    println(car1 in mixedSet1)
+//
+//    println()
+//    println("Set")
+//
+//    // mutable
+//    var intSet4: MutableSet<Int> = mutableSetOf(2,4,6,8,10)
+//    intSet4.add(12)
+//    intSet3.forEach(fun(value: Int) {
+//        if (value %2 == 0) {
+//            intSet4.add(value)
+//        }
+//    })
+//    intSet4.remove(50)
+//    println(intSet4)
+//
+//    val union = intSet4.union(intSet2)
+//    val intersection = intSet4.intersect(intSet2)
+//    val substract = intSet4 - intSet2
+//    val symDiff = (intSet2 - intSet4) union (intSet4 - intSet2)
+//
+//
+//
+//    println(union)
+//    println(intersection)
+//    println(symDiff.sorted())
+//    println(substract)
 
-    // set
-    // Set<*> is Immutable
-    // MutableSet<*> is Mutable
-    // same as list, but every content in it must be unique
-    val car1 = Car(
-        name = "Hyundai",
-        fuel = "Gasoline"
+    // Map
+    // Map<*> is Immutable
+    // MutableMap<*> is Mutable
+    val capital: Map<String, String> = mapOf(
+        "Jakarta" to "Indonesia",
+        "London" to "New York",
+        "New Delhi" to "India",
+        "Kuala Lumpur" to "Malaysia"
     )
-    val car2 = car1
+    // printing Map Value
+    println(capital)
+    println(capital["Jakarta"])
+    println(capital["ASDASGDHASD"]) // printing null when no data found
 
-    // immutable
-    val mixedSet1 = setOf(1, "True", 2.3, true, car1, car2) // cause car1 and car2 have the same reference, only one will included
-    val intSet1: Set<Int> = setOf(1, 2, 3, 4, 5, 6, 1, 2, 3, 4) // 1, 2, 3, 4, 5, 6
-    val intSet2: Set<Int> = setOf(5, 6, 1, 2, 3, 4) // 1, 2, 3, 4, 5, 6
-    val intSet3: Set<Int> = setOf(5, 10, 20, 30, 40, 50)
-
-    println(mixedSet1)
-    println(intSet1)
-    println(intSet2)
-    println(intSet3)
-    println(intSet2 == intSet1) // true
-    println(intSet2 == intSet3) // true
-    println(car1 in mixedSet1)
+    println(capital.getValue("Jakarta"))
+    try {
+        // throw Exception when no data found
+        println(capital.getValue("ASDASGDHASD"))
+    } catch (e: Exception) {
+        println(e)
+        println(e.message)
+    }
+    println()
+    println(capital.keys) // returning Set
+    println(capital.values) // returning Collection
+    println(capital.size) // returning Int
+    for((key, value) in capital) {
+        println("$key is the Capital of $value")
+    }
 
     println()
-    println("Set")
+    println("Mutable Map")
+    val mutableCapitals: MutableMap<String, String> = capital.toMutableMap()
+    val mutableTempCapitals: MutableMap<String, String> = mutableMapOf()
+    for ((key, value) in capital) {
+        mutableTempCapitals.put(value, key)
+    }
+    mutableCapitals.putAll(mutableTempCapitals)
+    println(mutableCapitals)
 
-    // mutable
-    var intSet4: MutableSet<Int> = mutableSetOf(2,4,6,8,10)
-    intSet4.add(12)
-    intSet3.forEach(fun(value: Int) {
-        if (value %2 == 0) {
-            intSet4.add(value)
-        }
-    })
-    intSet4.remove(50)
-    println(intSet4)
-
-    val union = intSet4.union(intSet2)
-    val intersection = intSet4.intersect(intSet2)
-    val substract = intSet4 - intSet2
-    val symDiff = (intSet2 - intSet4) union (intSet4 - intSet2)
-
-
-
-    println(union)
-    println(intersection)
-    println(symDiff.sorted())
-    println(substract)
 }
