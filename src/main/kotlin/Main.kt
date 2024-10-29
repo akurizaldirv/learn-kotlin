bb75bdf254069a5c2befcb8c33685bdb9bcbf2dc
Zaldi Irvana <akurizaldirv@gmail.com>
2024-10-29 09:03:19 +0700

    add set

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -44,31 +44,84 @@ infix fun Int.sumTenDeret(num: Int): Int {
 }
 
 fun main() {
-    // immutable
-    val mixList = listOf(1, "Hello", 'W', true, 2.4F, Car(
+//    // immutable
+    // List<*> is Immutable
+    // MutableList<*> is Mutable
+
+//    val mixList = listOf(1, "Hello", 'W', true, 2.4F, Car(
+//        name = "Hyundai",
+//        fuel = "Gasoline"
+//    ))
+//    val charList: List<Char> = listOf('A', 'B', 'C', 'D', 'E', 'F', 'G')
+//
+//     // mixList[1] = "World" // error
+//    println(mixList)
+//    println(charList)
+//    println(mixList[3])
+//    println(charList[2])
+//    // mixList.add("Error") // throw error -- unresolved reference add
+//
+//    // mutable
+//    println("\n\nMutable List")
+//    val gradeList: MutableList<Int> = mutableListOf<Int>(90, 70, 60)
+//    println(gradeList)
+//    gradeList.add(95)
+//    gradeList.add(75)
+//    gradeList.add(1, 88)
+//    gradeList.remove(60)
+//    gradeList.removeAt(gradeList.size -1)
+//    gradeList.add(77)
+//    gradeList[0] = 99
+//    println(gradeList)
+
+
+    // set
+    // Set<*> is Immutable
+    // MutableSet<*> is Mutable
+    // same as list, but every content in it must be unique
+    val car1 = Car(
         name = "Hyundai",
         fuel = "Gasoline"
-    ))
-    val charList: List<Char> = listOf('A', 'B', 'C', 'D', 'E', 'F', 'G')
+    )
+    val car2 = car1
 
-//     mixList[1] = "World" // error
-    println(mixList)
-    println(charList)
-    println(mixList[3])
-    println(charList[2])
-    // mixList.add("Error") // throw error -- unresolved reference add
+    // immutable
+    val mixedSet1 = setOf(1, "True", 2.3, true, car1, car2) // cause car1 and car2 have the same reference, only one will included
+    val intSet1: Set<Int> = setOf(1, 2, 3, 4, 5, 6, 1, 2, 3, 4) // 1, 2, 3, 4, 5, 6
+    val intSet2: Set<Int> = setOf(5, 6, 1, 2, 3, 4) // 1, 2, 3, 4, 5, 6
+    val intSet3: Set<Int> = setOf(5, 10, 20, 30, 40, 50)
+
+    println(mixedSet1)
+    println(intSet1)
+    println(intSet2)
+    println(intSet3)
+    println(intSet2 == intSet1) // true
+    println(intSet2 == intSet3) // true
+    println(car1 in mixedSet1)
+
+    println()
+    println("Set")
 
     // mutable
-    println("\n\nMutable List")
-    val gradeList = mutableListOf<Int>(90, 70, 60)
-    println(gradeList)
-    gradeList.add(95)
-    gradeList.add(75)
-    gradeList.add(1, 88)
-    gradeList.remove(60)
-    gradeList.removeAt(gradeList.size -1)
-    gradeList.add(77)
-    gradeList[0] = 99
-    println(gradeList)
+    var intSet4: MutableSet<Int> = mutableSetOf(2,4,6,8,10)
+    intSet4.add(12)
+    intSet3.forEach(fun(value: Int) {
+        if (value %2 == 0) {
+            intSet4.add(value)
+        }
+    })
+    intSet4.remove(50)
+    println(intSet4)
 
+    val union = intSet4.union(intSet2)
+    val intersection = intSet4.intersect(intSet2)
+    val substract = intSet4 - intSet2
+    val symDiff = (intSet2 - intSet4) union (intSet4 - intSet2)
+
+
+
+    println(union)
+    println(intersection)
+    println(symDiff.sorted())
+    println(substract)
 }
